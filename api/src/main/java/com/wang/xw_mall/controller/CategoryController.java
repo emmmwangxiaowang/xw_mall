d00b228921c0a722b587ccf9807f2c81514fcbc7
emmmwang <954544828@qq.com>
2022-10-27 01:06:15 +0800

    类别接口实现

--- api/src/main/java/com/wang/xw_mall/controller/CategoryController.java
+++ api/src/main/java/com/wang/xw_mall/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.wang.xw_mall.controller;
 
 
+import com.wang.xw_mall.service.ICategoryService;
+import com.wang.xw_mall.vo.ResultVo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
--- service/src/main/java/com/wang/xw_mall/service/ICategoryService.java
+++ service/src/main/java/com/wang/xw_mall/service/ICategoryService.java
@@ -1,7 +1,11 @@
 package com.wang.xw_mall.service;
 
+import com.wang.xw_mall.entity.CategoryVo;
 import com.wang.xw_mall.pojo.Category;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.wang.xw_mall.vo.ResultVo;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-10-11
  */
 public interface ICategoryService extends IService<Category> {
+
+    public ResultVo queryAllCategories(int categoryId);
 
 }
